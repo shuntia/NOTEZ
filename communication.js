0c9125a62f29ad4d694c1cf943ab22201e8cda33
Shuntia <128505810+shuntia@users.noreply.github.com>
2025-04-07 19:26:31 -0700

    add ices?

--- communication.js
+++ communication.js
@@ -1,7 +1,33 @@
 // Replace with your Render server URL (use wss:// for secure connections)
 const serverUrl = "https://notez-server.onrender.com";
 const socket = new WebSocket(serverUrl);
-const peerConnection = new RTCPeerConnection({ iceServers: [{ urls: "stun:stun.l.google.com:19302" }] });
+const peerConnection = new RTCPeerConnection({
+  iceServers: [{
+    url: 'turn:numb.viagenie.ca',
+    credential: 'muazkh',
+    username: 'webrtc@live.com'
+  },
+  {
+    url: 'turn:192.158.29.39:3478?transport=udp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+  },
+  {
+    url: 'turn:192.158.29.39:3478?transport=tcp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+  },
+  {
+    url: 'turn:turn.bistri.com:80',
+    credential: 'homeo',
+    username: 'homeo'
+  },
+  {
+    url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+    credential: 'webrtc',
+    username: 'webrtc'
+  }]
+});
 let dataChannel; // Will be set for both initiator and receiver
 var code, pass;
 let candidateBuffer = [];  // Buffer ICE candidates if remoteDescription isn’t set
