b948158ccbdf18e2559dd713e735e5eeb564c22a
Shuntia <128505810+shuntia@users.noreply.github.com>
2025-04-07 19:44:58 -0700

    i dont know at this point

--- communication.js
+++ communication.js
@@ -1,17 +1,18 @@
-// ...existing code replaced...
-const serverUrl = "https://notez-server.onrender.com";
+const serverUrl = "wss://notez-server.onrender.com";
 const socket = new WebSocket(serverUrl);
 
-const peerConnection = new RTCPeerConnection();
+const peerConnection = new RTCPeerConnection({
+  iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+});
 
 let dataChannel;
 let code, pass;
 let candidateBuffer = [];
 
-// Signaling server open
+// On open
 socket.onopen = () => console.log("Connected to server");
 
-// Handle server messages
+// Receive messages
 socket.onmessage = async (evt) => {
   let data = evt.data;
   if (data instanceof Blob) {
@@ -19,71 +20,72 @@ socket.onmessage = async (evt) => {
   }
   try {
     data = JSON.parse(data);
-  } catch (err) {
-    console.error("JSON parse error:", err);
+  } catch (error) {
+    console.error("JSON parse error:", error);
     return;
   }
   console.log("Received:", data);
 
-  // Offer
-  if (data.type === "offer") {
-    await peerConnection.setRemoteDescription(data.sdp);
-    flushCandidateBuffer();
+  switch (data.type) {
+    case "match":
+      console.log("Match found, initiator?", data.initiator);
+      // Only initiator calls startHandshake
+      if (data.initiator) {
+        startHandshake(code, pass);
+      }
+      break;
 
-    if (peerConnection.signalingState === "have-remote-offer") {
-      const answer = await peerConnection.createAnswer();
-      await peerConnection.setLocalDescription(answer);
-      socket.send(JSON.stringify({ type: "answer", sdp: peerConnection.localDescription, code, pass }));
-    }
-  }
+    case "newroom":
+      console.log("New room created");
+      break;
 
-  // Answer
-  else if (data.type === "answer") {
-    if (peerConnection.signalingState === "have-local-offer") {
+    case "offer":
       await peerConnection.setRemoteDescription(data.sdp);
       flushCandidateBuffer();
-    }
-  }
+      if (peerConnection.signalingState === "have-remote-offer") {
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        socket.send(
+          JSON.stringify({
+            type: "answer",
+            sdp: peerConnection.localDescription,
+            code,
+            pass
+          })
+        );
+      }
+      break;
 
-  // ICE candidate
-  else if (data.type === "candidate") {
-    if (!peerConnection.remoteDescription) {
-      candidateBuffer.push(data.candidate);
-    } else {
-      peerConnection.addIceCandidate(data.candidate).catch(console.error);
-    }
-  }
+    case "answer":
+      if (peerConnection.signalingState === "have-local-offer") {
+        await peerConnection.setRemoteDescription(data.sdp);
+        flushCandidateBuffer();
+      }
+      break;
 
-  // Match found
-  else if (data.type === "match") {
-    console.log("Match found, initiator?", data.initiator);
-    // If initiator, do something as needed
-  } else if (data.type === "newroom") {
-    console.log("New room created");
+    case "candidate":
+      if (!peerConnection.remoteDescription) {
+        candidateBuffer.push(data.candidate);
+      } else {
+        peerConnection.addIceCandidate(data.candidate).catch(console.error);
+      }
+      break;
   }
 };
 
-// Send local ICE candidates to the server
+// Send ICE candidates
 peerConnection.onicecandidate = (evt) => {
   if (evt.candidate) {
     socket.send(JSON.stringify({ type: "candidate", candidate: evt.candidate }));
   }
 };
 
-// If the other side creates a data channel
+// If other side creates a data channel
 peerConnection.ondatachannel = (evt) => {
   dataChannel = evt.channel;
   dataChannel.onopen = () => console.log("Data channel open (Receiver)");
   dataChannel.onmessage = (msg) => console.log("Receiver got:", msg.data);
 };
-
-// Flush buffered candidates
-function flushCandidateBuffer() {
-  candidateBuffer.forEach((cand) => {
-    peerConnection.addIceCandidate(cand).catch(console.error);
-  });
-  candidateBuffer = [];
-}
 
 // Initiator handshake
 function startHandshake(inputCode, inputPass) {
@@ -93,10 +95,19 @@ function startHandshake(inputCode, inputPass) {
   dataChannel.onopen = () => console.log("Data channel open (Initiator)");
   dataChannel.onmessage = (msg) => console.log("Initiator got:", msg.data);
 
-  peerConnection.createOffer()
+  peerConnection
+    .createOffer()
     .then((offer) => peerConnection.setLocalDescription(offer))
-    .then(() => {
-      socket.send(JSON.stringify({ type: "probe", data: { code, pass } }));
-    })
+    .then(() =>
+      socket.send(JSON.stringify({ type: "probe", data: { code, pass } }))
+    )
     .catch(console.error);
+}
+
+// Process buffered candidates
+function flushCandidateBuffer() {
+  candidateBuffer.forEach((cand) => {
+    peerConnection.addIceCandidate(cand).catch(console.error);
+  });
+  candidateBuffer = [];
 }
